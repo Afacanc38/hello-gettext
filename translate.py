556f8540fce096b946c4e3b31b0f30c8f9aa42c9
Alperen İsa Nalbant <66299502+Afacanc38@users.noreply.github.com>
2022-02-12 06:34:23 +0000

    Update translate.py

--- translate.py
+++ translate.py
@@ -5,7 +5,7 @@ except:
     print("\a\033[1;96minfo\033[0;1m: Exit code: 1\033[0m")
     exit(1)
 for lang in ['tr', 'en']:
-    lang_translations = gettext.translation('base', localedir='locales', languages=[lang] fallback=True)
+    lang_translations = gettext.translation('base', localedir='locales', languages=[lang], fallback=True)
     lang_translations.install()
     _ = lang_translations.gettext
 
