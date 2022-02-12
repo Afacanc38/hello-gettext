b4872314f639b835bd6c7de30a2b2d48c6ac6258
Alperen İsa Nalbant <66299502+Afacanc38@users.noreply.github.com>
2022-02-12 06:39:26 +0000

    Update translate.py

--- translate.py
+++ translate.py
@@ -4,9 +4,15 @@ except:
     print("\a\033[1;91merror\033[0;1m: Gettext (localization module) not found.")
     print("\a\033[1;96minfo\033[0;1m: Exit code: 1\033[0m")
     exit(1)
-for lang in ['tr', 'en']:
-    lang_translations = gettext.translation('base', localedir='locales', languages=[lang], fallback=True)
-    lang_translations.install()
-    _ = lang_translations.gettext
 
+envkey = 'LANG'
+lang = os.getenv(envkey)
+if 'tr' in lang:
+    tr = gettext.translation('base', localedir='locales', languages=['tr'])
+    tr.install()
+    _ = tr.gettext
+else:
+    en = gettext.translation('base', localedir='locales', languages=['en'])
+    en.install()
+    _ = en.gettext
 os.system("rm -rf __pycache__")
